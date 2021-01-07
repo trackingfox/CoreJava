4544d5a98728bf05a6a61236ec536a77d37e0e48
trackingfox <sharifali1993@gmail.com>
2021-01-07 19:45:12 +0530

    Changed file Commit.

--- src/com/codingbat/Q50_100.java
+++ src/com/codingbat/Q50_100.java
@@ -64,4 +64,102 @@ public class Q50_100 {
 	 * "<<WooHoo>>" makeOutWord("[[]]", "word") → "[[word]]"
 	 */
 
+	public String makeOutWord(String out, String word) {
+
+		return word;
+
+	}
+
+	/*
+	 * Given a string, return a new string made of 3 copies of the last 2 chars of
+	 * the original string. The string length will be at least 2.
+	 * 
+	 * 
+	 * extraEnd("Hello") → "lololo" extraEnd("ab") → "ababab" extraEnd("Hi") →
+	 * "HiHiHi"
+	 */
+
+	public String extraEnd(String str) {
+
+		String sub = str.substring(str.length() - 2);
+		return sub + sub + sub;
+
+	}
+
+	@Test
+	public void extraEndtest() {
+		Assert.assertEquals(extraEnd("Hello"), "lololo");
+		Assert.assertEquals(extraEnd("ab"), "ababab");
+		Assert.assertEquals(extraEnd("Hi"), "HiHiHi");
+
+	}
+
+	/*
+	 * Given a string, return the string made of its first two chars, so the String
+	 * "Hello" yields "He". If the string is shorter than length 2, return whatever
+	 * there is, so "X" yields "X", and the empty string "" yields the empty string
+	 * "". Note that str.length() returns the length of a string.
+	 * 
+	 * 
+	 * firstTwo("Hello") → "He" firstTwo("abcdefg") → "ab" firstTwo("ab") → "ab"
+	 */
+
+	public String firstTwo(String str) {
+
+		return str.length() <= 2 ? str.substring(0) : str.substring(0, 2);
+
+	}
+
+	@Test
+	public void firstTwotest() {
+		Assert.assertEquals(firstTwo("Hello"), "He");
+		Assert.assertEquals(firstTwo("abcdefg"), "ab");
+		Assert.assertEquals(firstTwo("ab"), "ab");
+
+	}
+
+	/*
+	 * Given a string of even length, return the first half. So the string "WooHoo"
+	 * yields "Woo".
+	 * 
+	 * 
+	 * firstHalf("WooHoo") → "Woo" firstHalf("HelloThere") → "Hello"
+	 * firstHalf("abcdef") → "abc"
+	 */
+
+	public String firstHalf(String str) {
+
+		int end = str.length();
+		int len = 0;
+		String y = "";
+		if (end % 2 == 0)
+			len = end / 2;
+		else
+			len = (end + 1) / 2;
+
+		for (int i = 0; i < len; i++) {
+			y = y + str.charAt(i);
+		}
+
+		return y;
+
+	}
+
+	@Test
+	public void firstHalftest() {
+		Assert.assertEquals(firstHalf("WooHoo"), "Woo");
+		Assert.assertEquals(firstHalf("HelloThere"), "Hello");
+		Assert.assertEquals(firstHalf("abcdef"), "abc");
+
+	}
+
+	/*
+	 * Given a string, return a version without the first and last char, so "Hello"
+	 * yields "ell". The string length will be at least 2.
+	 * 
+	 * 
+	 * withoutEnd("Hello") → "ell" withoutEnd("java") → "av" withoutEnd("coding") →
+	 * "odin"
+	 */
+
 }
