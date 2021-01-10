bbb7e28096803249b78ebc4ea97500d5c503a8d8
trackingfox <sharifali1993@gmail.com>
2021-01-10 19:00:01 +0530

    Changed file Commit.

--- src/com/codingbat/Q50_100.java
+++ src/com/codingbat/Q50_100.java
@@ -164,8 +164,212 @@ public class Q50_100 {
 
 	public String withoutEnd(String str) {
 
-		return str;
+		int end = str.length();
+		return str.substring(1, end - 1);
 
+	}
+
+	@Test
+	public void withoutEndtest() {
+		Assert.assertEquals(withoutEnd("Hello"), "ell");
+		Assert.assertEquals(withoutEnd("java"), "av");
+		Assert.assertEquals(withoutEnd("coding"), "odin");
+
+	}
+
+	/*
+	 * Given 2 strings, a and b, return a string of the form short+long+short, with
+	 * the shorter string on the outside and the longer string on the inside. The
+	 * strings will not be the same length, but they may be empty (length 0).
+	 * 
+	 * 
+	 * comboString("Hello", "hi") → "hiHellohi" comboString("hi", "Hello") →
+	 * "hiHellohi" comboString("aaa", "b") → "baaab"
+	 */
+
+	public String comboString(String a, String b) {
+
+		return a.length() > b.length() ? b + a + b : a + b + a;
+
+	}
+
+	@Test
+	public void comboStringtest() {
+		Assert.assertEquals(comboString("Hello", "hi"), "hiHellohi");
+		Assert.assertEquals(comboString("hi", "Hello"), "hiHellohi");
+		Assert.assertEquals(comboString("aaa", "b"), "baaab");
+
+	}
+
+	/*
+	 * Given 2 strings, return their concatenation, except omit the first char of
+	 * each. The strings will be at least length 1.
+	 * 
+	 * 
+	 * nonStart("Hello", "There") → "ellohere" nonStart("java", "code") → "avaode"
+	 * nonStart("shotl", "java") → "hotlava"
+	 */
+
+	public String nonStart(String a, String b) {
+		return a.substring(1) + b.substring(1);
+	}
+
+	@Test
+	public void nonStarttest() {
+		Assert.assertEquals(nonStart("Hello", "There"), "ellohere");
+		Assert.assertEquals(nonStart("java", "code"), "avaode");
+		Assert.assertEquals(nonStart("shotl", "java"), "hotlava");
+
+	}
+
+	/*
+	 * Given a string, return a "rotated left 2" version where the first 2 chars are
+	 * moved to the end. The string length will be at least 2.
+	 * 
+	 * 
+	 * left2("Hello") → "lloHe" left2("java") → "vaja" left2("Hi") → "Hi"
+	 */
+
+	public String left2(String str) {
+
+		return str.substring(2) + str.substring(0, 2);
+	}
+
+	/*
+	 * Given a string, return a "rotated right 2" version where the last 2 chars are
+	 * moved to the start. The string length will be at least 2.
+	 * 
+	 * 
+	 * right2("Hello") → "loHel" right2("java") → "vaja" right2("Hi") → "Hi"
+	 * 
+	 */
+
+	public String right2(String str) {
+		return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+	}
+
+	/*
+	 * Given a string, return a string length 1 from its front, unless front is
+	 * false, in which case return a string length 1 from its back. The string will
+	 * be non-empty.
+	 * 
+	 * 
+	 * theEnd("Hello", true) → "H" theEnd("Hello", false) → "o" theEnd("oh", true) →
+	 * "o"
+	 */
+
+	public String theEnd(String str, boolean front) {
+
+		if (front) {
+
+			return str.substring(0, 1);
+
+		} else
+			return str.substring(str.length() - 1);
+
+	}
+
+	@Test
+	public void theEndtest() {
+		Assert.assertEquals(theEnd("Hello", true), "H");
+		Assert.assertEquals(theEnd("Hello", false), "o");
+		Assert.assertEquals(theEnd("oh", true), "o");
+
+	}
+
+	/*
+	 * Given a string, return a version without both the first and last char of the
+	 * string. The string may be any length, including 0.
+	 * 
+	 * 
+	 * withouEnd2("Hello") → "ell" withouEnd2("abc") → "b" withouEnd2("ab") → ""
+	 * 
+	 */
+
+	public String withouEnd2(String str) {
+
+		if (str.length() > 1)
+			return str.substring(1, str.length() - 1);
+		else
+			return "";
+
+	}
+
+	/*
+	 * Given a string of even length, return a string made of the middle two chars,
+	 * so the string "string" yields "ri". The string length will be at least 2.
+	 * 
+	 * 
+	 * middleTwo("string") → "ri" middleTwo("code") → "od" middleTwo("Practice") →
+	 * "ct"
+	 */
+
+	public String middleTwo(String str) {
+
+		if (str.length() >= 2) {
+			return str.substring((str.length() / 2) - 1, (str.length() / 2) + 1);
+
+		} else
+			return "";
+
+	}
+
+	@Test
+	public void middleTwotest() {
+		Assert.assertEquals(middleTwo("string"), "ri");
+		Assert.assertEquals(middleTwo(""), "");
+		Assert.assertEquals(middleTwo("Practice"), "ct");
+
+	}
+
+	/*
+	 * Given a string, return true if it ends in "ly".
+	 * 
+	 * 
+	 * endsLy("oddly") → true endsLy("y") → false endsLy("oddy") → false
+	 */
+	public boolean endsLy(String str) {
+		if (str.length() >= 2)
+			return str.substring(str.length() - 2).equals("ly") ? true : false;
+		else
+			return false;
+	}
+
+	/*
+	 * Given a string and an int n, return a string made of the first and last n
+	 * chars from the string. The string length will be at least n.
+	 * 
+	 * 
+	 * nTwice("Hello", 2) → "Helo" nTwice("Chocolate", 3) → "Choate"
+	 * nTwice("Chocolate", 1) → "Ce"
+	 */
+
+	public String nTwice(String str, int n) {
+
+		return str.substring(0, n) + str.substring(str.length() - n);
+	}
+
+	/*
+	 * Given a string and an index, return a string length 2 starting at the given
+	 * index. If the index is too big or too small to define a string length 2, use
+	 * the first 2 chars. The string length will be at least 2.
+	 * 
+	 * 
+	 * twoChar("java", 0) → "ja" twoChar("java", 2) → "va" twoChar("java", 3) → "ja"
+	 */
+
+	/*
+	 * Given a string of odd length, return the string length 3 from its middle, so
+	 * "Candy" yields "and". The string length will be at least 3.
+	 * 
+	 * 
+	 * middleThree("Candy") → "and" middleThree("and") → "and"
+	 * middleThree("solving") → "lvi"
+	 */
+
+	public String middleThree(String str) {
+
+		return str.substring(((str.length() - 1) / 2) - 1, ((str.length() - 1) / 2) + 2);
 	}
 
 }
