51896773af0e94a763c5eae7f825e01850b102ab
trackingfox <sharifali1993@gmail.com>
2021-01-17 11:00:02 +0530

    Changed file Commit.

--- src/com/codingbat/Array1.java
+++ src/com/codingbat/Array1.java
@@ -1,9 +1,5 @@
 package com.codingbat;
 
-import org.junit.Test;
-
-import junit.framework.Assert;
-
 public class Array1 {
 
 	/*
@@ -122,14 +118,14 @@ public class Array1 {
 
 	}
 
-	@Test
-	public void rotateLeft3test() {
-		Assert.assertEquals(rotateLeft3(new int[] {1,2,3 }), {3,2,1});
-		Assert.assertEquals(rotateLeft3(new int[] { 5,11,9 }), {9,11,5});
-		Assert.assertEquals(rotateLeft3(new int[] { 7,0,0}), {0,0,7});
-		Assert.assertEquals(rotateLeft3(new int[] { 1,2,3 }), {3,2,1});
-
-	}
+//	@Test
+//	public void rotateLeft3test() {
+//		Assert.assertEquals(rotateLeft3(new int[] {1,2,3 }), {3,2,1});
+//		Assert.assertEquals(rotateLeft3(new int[] { 5,11,9 }), {9,11,5});
+//		Assert.assertEquals(rotateLeft3(new int[] { 7,0,0}), {0,0,7});
+//		Assert.assertEquals(rotateLeft3(new int[] { 1,2,3 }), {3,2,1});
+//
+//	}
 
 	/*
 	 * Given an array of ints length 3, return a new array with the elements in
@@ -152,11 +148,153 @@ public class Array1 {
 
 	}
 
-	@Test
-	public void reverse3test() {
-		Assert.assertEquals(reverse3(new int[] {1,2,3 }), {3,2,1});
-		Assert.assertEquals(rotateLeft3(new int[] { 5,11,9 }), {9,11,5});
-		Assert.assertEquals(rotateLeft3(new int[] { 7,0,0}), {0,0,7});
-		
+//	@Test
+//	public void reverse3test() {
+//		Assert.assertEquals(reverse3(new int[] {1,2,3 }), {3,2,1});
+//		Assert.assertEquals(rotateLeft3(new int[] { 5,11,9 }), {9,11,5});
+//		Assert.assertEquals(rotateLeft3(new int[] { 7,0,0}), {0,0,7});
+//		
+//	}
+
+	/*
+	 * Given an array of ints length 3, figure out which is larger, the first or
+	 * last element in the array, and set all the other elements to be that value.
+	 * Return the changed array.
+	 * 
+	 * 
+	 * maxEnd3([1, 2, 3]) → [3, 3, 3] maxEnd3([11, 5, 9]) → [11, 11, 11] maxEnd3([2,
+	 * 11, 3]) → [3, 3, 3]
+	 */
+
+	public int[] maxEnd3(int[] nums) {
+
+		int len = nums.length;
+		int[] arr = new int[len];
+
+		for (int i = 0; i < len; i++) {
+			if (nums[0] >= nums[len - 1]) {
+				arr[i] = nums[0];
+			} else
+				arr[i] = nums[len - 1];
+		}
+
+		return arr;
 	}
+
+	/*
+	 * Given an array of ints, return the sum of the first 2 elements in the array.
+	 * If the array length is less than 2, just sum up the elements that exist,
+	 * returning 0 if the array is length 0.
+	 * 
+	 * 
+	 * sum2([1, 2, 3]) → 3 sum2([1, 1]) → 2 sum2([1, 1, 1, 1]) → 2
+	 */
+
+	public int sum2(int[] nums) {
+
+		if (nums.length == 1)
+			return nums[0];
+
+		if (nums.length >= 1)
+			return nums[0] + nums[1];
+
+		return 0;
+	}
+
+	/*
+	 * Given 2 int arrays, a and b, each length 3, return a new array length 2
+	 * containing their middle elements.
+	 * 
+	 * 
+	 * middleWay([1, 2, 3], [4, 5, 6]) → [2, 5] middleWay([7, 7, 7], [3, 8, 0]) →
+	 * [7, 8]
+	 *
+	 * middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+	 */
+
+	public int[] middleWay(int[] a, int[] b) {
+
+		int len = a.length;
+		int[] arr = new int[2];
+		arr[0] = a[(len - 1) / 2];
+		arr[1] = b[(len - 1) / 2];
+		return arr;
+
+	}
+
+	/*
+	 * Given an array of ints, return a new array length 2 containing the first and
+	 * last elements from the original array. The original array will be length 1 or
+	 * more.
+	 * 
+	 * 
+	 * makeEnds([1, 2, 3]) → [1, 3] makeEnds([1, 2, 3, 4]) → [1, 4] makeEnds([7, 4,
+	 * 6, 2]) → [7, 2]
+	 */
+
+	public int[] makeEnds(int[] nums) {
+
+		int len = nums.length;
+		int[] arr = new int[2];
+		if (len > 1) {
+			arr[0] = nums[0];
+			arr[1] = nums[(len - 1)];
+			return arr;
+		}
+		if (len == 1) {
+			arr[0] = nums[0];
+			return arr;
+		}
+
+		return arr;
+
+	}
+
+	/*
+	 * Given an int array length 2, return true if it contains a 2 or a 3.
+	 * 
+	 * 
+	 * has23([2, 5]) → true has23([4, 3]) → true has23([4, 5]) → false
+	 */
+
+	public boolean has23(int[] nums) {
+
+		return false;
+
+	}
+
+	/*
+	 * Given an int array, return a new array with double the length where its last
+	 * element is the same as the original array, and all the other elements are 0.
+	 * The original array will be length 1 or more. Note: by default, a new int
+	 * array contains all 0's.
+	 * 
+	 * 
+	 * makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6] makeLast([1, 2]) → [0, 0, 0, 2]
+	 * makeLast([3]) → [0, 3]
+	 */
+
+	public int[] makeLast(int[] nums) {
+
+		int len = nums.length;
+		int[] arr = new int[2 * len];
+
+		arr[2 * len - 1] = nums[len - 1];
+		for (int i = 0; i < 2 * len - 1; i++) {
+
+			arr[i] = 0;
+		}
+
+		return arr;
+	}
+
+	/*
+	 * Given an int array, return true if the array contains 2 twice, or 3 twice.
+	 * The array will be length 0, 1, or 2.
+	 * 
+	 * 
+	 * double23([2, 2]) → true double23([3, 3]) → true double23([2, 3]) → false
+	 */
+
+	// List<Integer> list3 = Arrays.asList(myIntegerArray)
 }
