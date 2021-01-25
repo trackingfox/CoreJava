4b32b3087d84548479b7f67fb1ce76a47421245a
trackingfox <sharifali1993@gmail.com>
2021-01-25 23:46:34 +0530

    Changed file Commit.

--- src/com/codingbat/String2.java
+++ src/com/codingbat/String2.java
@@ -26,4 +26,41 @@ public class String2 {
 	 * countHi("abc hi ho") → 1 countHi("ABChi hi") → 2 countHi("hihi") → 2
 	 * 
 	 */
+
+	public int countHi(String str) {
+		int count = 0;
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.substring(i, i + 2).equals("hi"))
+				count = count + 1;
+		}
+		return count;
+	}
+
+	/*
+	 * Return true if the string "cat" and "dog" appear the same number of times in
+	 * the given string.
+	 * 
+	 * 
+	 * catDog("catdog") → true catDog("catcat") → false catDog("1cat1cadodog") →
+	 * true
+	 */
+
+	public boolean catDog(String str) {
+		int count = 0;
+		for (int i = 0; i < str.length() - 2; i++) {
+			if (str.substring(i, i + 3).equals("cat")) {
+				count++;
+			}
+			if (str.substring(i, i + 3).equals("dog")) {
+				count--;
+			}
+		}
+		if (count == 0) {
+			return true;
+		}
+
+		return false;
+
+	}
+
 }
