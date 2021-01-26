c20b42d0195f655ce79c515fdab5d51e04b899c5
trackingfox <sharifali1993@gmail.com>
2021-01-26 19:00:02 +0530

    Changed file Commit.

--- src/com/codingbat/String2.java
+++ src/com/codingbat/String2.java
@@ -63,4 +63,15 @@ public class String2 {
 
 	}
 
+	/*
+	 * Given two strings, return true if either of the strings appears at the very
+	 * end of the other string, ignoring upper/lower case differences (in other
+	 * words, the computation should not be "case sensitive"). Note:
+	 * str.toLowerCase() returns the lowercase version of a string.
+	 * 
+	 * 
+	 * endOther("Hiabc", "abc") → true endOther("AbC", "HiaBc") → true
+	 * endOther("abc", "abXabc") → true
+	 */
+
 }
