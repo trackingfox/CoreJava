33d8fad2f84d671bb76579c6cb5945fcc4857cbc
trackingfox <sharifali1993@gmail.com>
2021-01-26 22:50:54 +0530

    Changed file Commit.

--- src/com/codingbat/String2.java
+++ src/com/codingbat/String2.java
@@ -64,14 +64,21 @@ public class String2 {
 	}
 
 	/*
-	 * Given two strings, return true if either of the strings appears at the very
-	 * end of the other string, ignoring upper/lower case differences (in other
-	 * words, the computation should not be "case sensitive"). Note:
-	 * str.toLowerCase() returns the lowercase version of a string.
+	 * Given a string and an int n, return a string made of n repetitions of the
+	 * last n characters of the string. You may assume that n is between 0 and the
+	 * length of the string, inclusive.
 	 * 
 	 * 
-	 * endOther("Hiabc", "abc") → true endOther("AbC", "HiaBc") → true
-	 * endOther("abc", "abXabc") → true
+	 * repeatEnd("Hello", 3) → "llollollo" repeatEnd("Hello", 2) → "lolo"
+	 * repeatEnd("Hello", 1) → "o"
 	 */
+
+	public String repeatEnd(String str, int n) {
+		StringBuilder stbuild = new StringBuilder(n * n);
+		String last = str.substring(str.length() - n);
+		for (int i = 0; i < n; i++)
+			stbuild.append(last);
+		return stbuild.toString();
+	}
 
 }
