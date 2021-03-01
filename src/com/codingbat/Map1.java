baa71a12655059f4699629b6b414f57392f9821d
trackingfox <sharifali1993@gmail.com>
2021-03-01 23:58:06 +0530

    Changed file Commit.

--- src/com/codingbat/Map1.java
+++ src/com/codingbat/Map1.java
@@ -490,4 +490,32 @@ public class Map1 {
 		return strings;
 	}
 
+	/*
+	 * Given an array of ints, is it possible to choose a group of some of the ints,
+	 * such that the group sums to the given target, with this additional
+	 * constraint: if there are numbers in the array that are adjacent and the
+	 * identical value, they must either all be chosen, or none of them chosen. For
+	 * example, with the array {1, 2, 2, 2, 5, 2}, either all three 2's in the
+	 * middle must be chosen or not, all as a group. (one loop can be used to find
+	 * the extent of the identical values).
+	 * 
+	 * 
+	 * groupSumClump(0, [2, 4, 8], 10) → true groupSumClump(0, [1, 2, 4, 8, 1], 14)
+	 * → true groupSumClump(0, [2, 4, 4, 8], 14) → false
+	 */
+
+	public boolean groupSumClump(int start, int[] nums, int target) {
+		if (start >= nums.length) {
+			if (target == 0)
+				return true;
+			return false;
+		}
+		int i = start + 1;
+		for (; i < nums.length && nums[start] == nums[i]; i++)
+			;
+		if (groupSumClump(i, nums, target - ((i - start) * nums[start])))
+			return true;
+		return groupSumClump(i, nums, target);
+	}
+
 }
