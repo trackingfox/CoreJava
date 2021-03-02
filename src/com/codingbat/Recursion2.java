bec92e11ef3e1406e8464e7b26f82f2618cbbf6d
trackingfox <sharifali1993@gmail.com>
2021-03-02 23:55:25 +0530

    Changed file Commit.

--- src/com/codingbat/Recursion2.java
+++ src/com/codingbat/Recursion2.java
@@ -75,14 +75,109 @@ public class Recursion2 {
 
 	/*
 	 * Given an array of ints, is it possible to choose a group of some of the ints,
-	 * such that the group sums to the given target with these additional
-	 * constraints: all multiples of 5 in the array must be included in the group.
-	 * If the value immediately following a multiple of 5 is 1, it must not be
-	 * chosen. (No loops needed.)
+	 * such that the group sums to the given target, with this additional
+	 * constraint: if there are numbers in the array that are adjacent and the
+	 * identical value, they must either all be chosen, or none of them chosen. For
+	 * example, with the array {1, 2, 2, 2, 5, 2}, either all three 2's in the
+	 * middle must be chosen or not, all as a group. (one loop can be used to find
+	 * the extent of the identical values).
 	 * 
 	 * 
-	 * groupSum5(0, [2, 5, 10, 4], 19) → true groupSum5(0, [2, 5, 10, 4], 17) → true
-	 * groupSum5(0, [2, 5, 10, 4], 12) → false
+	 * groupSumClump(0, [2, 4, 8], 10) → true groupSumClump(0, [1, 2, 4, 8, 1], 14)
+	 * → true groupSumClump(0, [2, 4, 4, 8], 14) → false
 	 */
+
+	public boolean groupSumClump(int start, int[] nums, int target) {
+		if (start >= nums.length) {
+			if (target == 0)
+				return true;
+			return false;
+		}
+		int i = start + 1;
+		for (; i < nums.length && nums[start] == nums[i]; i++)
+			;
+		if (groupSumClump(i, nums, target - ((i - start) * nums[start])))
+			return true;
+		return groupSumClump(i, nums, target);
+	}
+
+	/*
+	 * Given an array of ints, is it possible to divide the ints into two groups, so
+	 * that the sums of the two groups are the same. Every int must be in one group
+	 * or the other. Write a recursive helper method that takes whatever arguments
+	 * you like, and make the initial call to your recursive helper from
+	 * splitArray(). (No loops needed.)
+	 * 
+	 * 
+	 * splitArray([2, 2]) → true splitArray([2, 3]) → false splitArray([5, 2, 3]) →
+	 * true
+	 */
+
+	public boolean splitArray(int[] nums) {
+		return sidesAreEqual(nums, 0, 0);
+	}
+
+	// recursive helper method
+	public boolean sidesAreEqual(int[] nums, int i, int balance) {
+		if (i == nums.length)
+			return (balance == 0);
+		if (sidesAreEqual(nums, i + 1, balance + nums[i]))
+			return true;
+		return sidesAreEqual(nums, i + 1, balance - nums[i]);
+	}
+
+	/*
+	 * Given an array of ints, is it possible to divide the ints into two groups, so
+	 * that the sum of one group is a multiple of 10, and the sum of the other group
+	 * is odd. Every int must be in one group or the other. Write a recursive helper
+	 * method that takes whatever arguments you like, and make the initial call to
+	 * your recursive helper from splitOdd10(). (No loops needed.)
+	 * 
+	 * 
+	 * splitOdd10([5, 5, 5]) → true splitOdd10([5, 5, 6]) → false splitOdd10([5, 5,
+	 * 6, 1]) → true
+	 */
+
+	public boolean splitOdd10(int[] nums) {
+		return sidesAreOdd10(nums, 0, 0, 0);
+	}
+
+	// // recursive helper method
+	public boolean sidesAreOdd10(int[] nums, int i, int group1, int group2) {
+		if (i == nums.length)
+			return (group1 % 2 == 1 && group2 % 10 == 0 || group2 % 2 == 1 && group1 % 10 == 0);
+		if (sidesAreOdd10(nums, i + 1, group1 + nums[i], group2))
+			return true;
+		return sidesAreOdd10(nums, i + 1, group1, group2 + nums[i]);
+	}
+
+	/*
+	 * Given an array of ints, is it possible to divide the ints into two groups, so
+	 * that the sum of the two groups is the same, with these constraints: all the
+	 * values that are multiple of 5 must be in one group, and all the values that
+	 * are a multiple of 3 (and not a multiple of 5) must be in the other. (No loops
+	 * needed.)
+	 * 
+	 * 
+	 * split53([1, 1]) → true split53([1, 1, 1]) → false split53([2, 4, 2]) → true
+	 * 
+	 */
+
+	public boolean split53(int[] nums) {
+		return sidesAreEqual53(nums, 0, 0);
+	}
+
+	// // recursive helper method
+	public boolean sidesAreEqual53(int[] nums, int i, int balance) {
+		if (i == nums.length)
+			return (balance == 0);
+		if (nums[i] % 5 == 0)
+			return sidesAreEqual53(nums, i + 1, balance + nums[i]);
+		if (nums[i] % 3 == 0)
+			return sidesAreEqual53(nums, i + 1, balance - nums[i]);
+		if (sidesAreEqual53(nums, i + 1, balance + nums[i]))
+			return true;
+		return sidesAreEqual53(nums, i + 1, balance - nums[i]);
+	}
 
 }
