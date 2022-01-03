ac372c4f802d2b2ff8e0ff2c60b00e6cebe6082d
trackingfox <sharifali1993@gmail.com>
2022-01-04 00:02:42 +0530

    Changed file Commit.

--- src/Prograimiz/SinglyLinkedList.java
+++ src/Prograimiz/SinglyLinkedList.java
@@ -150,6 +150,32 @@ public class SinglyLinkedList {
 		return previous;
 	}
 
+	public void printLinkedList() {
+		if (head == null) {
+			System.out.println("null");
+		}
+		ListNode current = head;
+		while (null != current) {
+			System.out.print(current.data + " --> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+
+	public ListNode getMiddleNode() {
+		if (head == null) {
+			return null;
+		}
+		ListNode slowPtr = head;
+		ListNode fastPtr = head;
+
+		while (fastPtr != null && fastPtr.next != null) {
+			slowPtr = slowPtr.next;
+			fastPtr = fastPtr.next.next;
+		}
+		return slowPtr;
+	}
+
 	public static void main(String[] args) {
 
 		SinglyLinkedList sll = new SinglyLinkedList();
