883ad7e65b497ee162cd82e700f5df79bdf3660c
trackingfox <sharifali1993@gmail.com>
2022-01-24 23:11:20 +0530

    Changed file Commit.

--- src/Prograimiz/Queue.java
+++ src/Prograimiz/Queue.java
@@ -1,5 +1,7 @@
 package Prograimiz;
 
+import java.util.NoSuchElementException;
+
 public class Queue {
 
 	private ListNode front;
@@ -24,8 +26,73 @@ public class Queue {
 		return length == 0;
 	}
 
+	public void enqueue(int data) {
+		ListNode temp = new ListNode(data);
+		if (isEmpty()) {
+			front = temp;
+		} else {
+			rear.next = temp;
+		}
+
+		rear = temp;
+		length++;
+	}
+
+	public void print() {
+		if (isEmpty()) {
+			return;
+		}
+		ListNode current = front;
+		while (current != null) {
+			System.out.print(current.data + "---> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+
+	public int dequeue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is already empty");
+		}
+
+		int result = front.data;
+		front = front.next;
+
+		if (front == null) {
+			rear = null;
+		}
+
+		length--;
+		return result;
+	}
+
+	public int first() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is already empty");
+		}
+		return front.data;
+	}
+
+	public int last() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("Queue is already empty");
+		}
+		return rear.data;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Queue queue = new Queue();
+		queue.enqueue(10);
+		queue.enqueue(15);
+		queue.enqueue(1);
+
+		queue.print();
+
+		queue.dequeue();
+		queue.print();
+
+		System.out.println(queue.first());
+		System.out.println(queue.last());
 
 	}
 
