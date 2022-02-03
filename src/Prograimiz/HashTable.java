dd19dda218030ed7d07d78f0d6ab0d4785ee18cc
trackingfox <sharifali1993@gmail.com>
2022-02-03 08:11:08 +0530

    Changed file Commit.

--- src/Prograimiz/HashTable.java
+++ src/Prograimiz/HashTable.java
@@ -35,20 +35,92 @@ public class HashTable {
 		return size == 0;
 	}
 
+//PUT
 	public void put(Integer key, String value) {
 		if (key == null || value == null) {
 			throw new IllegalArgumentException("Key or value is null !!!");
 		}
 		int bucketIndex = getBucketIndex(key);
+		HashNode head = buckets[bucketIndex];
+		while (head != null) {
+			if (head.key.equals(key)) {
+				head.value = value;
+				return;
+			}
+
+			head = head.next;
+
+		}
+		size++;
+		head = buckets[bucketIndex];
+		HashNode node = new HashNode(key, value); // (key, value) -> null
+		node.next = head;
+	}
+
+//BUCKET INDEX
+	private int getBucketIndex(Integer key) {
+		return key % numOfBuckets; // buckets.length
 
 	}
 
+	// GET
 	public String get(Integer key) {
+		if (key == null) {
+			throw new IllegalArgumentException("Key is null !!!");
+		}
+		int bucketIndex = getBucketIndex(key);
+		HashNode head = buckets[bucketIndex];
+
+		while (head != null) {
+			if (head.key.equals(key)) {
+
+				return head.value;
+			}
+
+			head = head.next;
+
+		}
 		return null;
 	}
 
+	// REMOVE
 	public String remove(Integer key) {
-		return null;
+
+		if (key == null) {
+			throw new IllegalArgumentException("Key is null !!!");
+		}
+
+		int bucketIndex = getBucketIndex(key);
+		HashNode head = buckets[bucketIndex];
+		HashNode previous = null;
+
+		while (head != null) {
+			if (head.key.equals(key)) {
+				break;
+			}
+			previous = head;
+			head = head.next;
+		}
+		if (head == null) {
+			return null;
+		}
+		size--;
+		if (previous != null) {
+			previous.next = head.next;
+		} else {
+			buckets[bucketIndex] = head.next;
+		}
+		return head.value;
+	}
+
+	public static void main(String[] args) {
+		HashTable table = new HashTable(10);
+		table.put(105, "Tom");
+		table.put(21, "Sana");
+		table.put(21, "Harry");
+		table.put(41, "Dinesh");
+		System.out.println(table.size());
+		System.out.println(table.get(31));
 	}
 
 }
